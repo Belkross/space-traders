89cb0fb398fda4b2b50131ae4410355f2e1f8fb4
belkross <belkross.pro@gmail.com>
2024-05-12 16:33:27 +0200

    fix: allow jest to support .js extensions

--- library/domain/jest.config.ts
+++ library/domain/jest.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   roots: ["./test"],
+
+  // allow jest to handle the .js extensions in .ts files
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 }
 
 export default config
--- library/domain/test/first.test.ts
+++ library/domain/test/first.test.ts
@@ -1,8 +1,30 @@
-/* function createAgent(username: string):  {
+import { InvalidUsernameError } from "../src/error/index.js"
+import { PostAgentDTO } from "../src/repository/space-traders.schema.js"
 
+function createAgent({
+  username,
+  validator,
+}: {
+  username: string
+  validator: (username: string) => boolean
+}): Promise<PostAgentDTO> {
+  /* if (validator(username) === false) throw new InvalidUsernameError()
+  return true as any */
+
+  throw new InvalidUsernameError()
 }
 
-test("test", () => {
-  expect(true).toBe(true)
+describe(createAgent.name, () => {
+  test("should be defined", () => {
+    expect(createAgent).toBeDefined()
+  })
+
+  test("should throw if the username don’t pass the validator", async () => {
+    const dummyUsername = "username"
+    const validator = jest.fn().mockReturnValue(false)
+
+    const agentCreation = async () => await createAgent({ username: dummyUsername, validator })
+
+    expect(agentCreation).rejects.toThrow(InvalidUsernameError)
+  })
 })
- */
